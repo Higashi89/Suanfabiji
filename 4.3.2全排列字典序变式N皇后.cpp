c088a37aebb9cc9b75c6b394c629f9040e57df84
Higashi89 <zxd1901@126.com>
2019-08-26 16:34:12 +0800

    zxd

--- 4.3.2全排列字典序变式N皇后.cpp
+++ 4.3.2全排列字典序变式N皇后.cpp
@@ -1,38 +1,37 @@
+﻿#include<cstdio>
+#include<cstring>
 #include<algorithm>
-#include<cstdio>
+#include<cmath>
 #include<iostream>
 using namespace std;
-int main(){
-    int N,max_amount;
-    cin>>N>>max_amount;
-    float k[N+1],p[N+1],s[N+1];
-    for(int i=0;i<N;i++){
-        scanf("%f",&k[i]);
-    }
-    for(int i=0;i<N;i++){
-        scanf("%f",&p[i]);
-        s[i]=p[i]/k[i];
-    }
-    int m=0;
-    float tp=0;
-    while(m<max_amount){
-        float max_s=0;int pos=0;//max_s一开始声明成了int类型，在最大值比较时出错
-        for(int i=0;i<N;i++){
-            if(s[i]>max_s){
-                max_s=s[i];pos=i;
+int N=8,counter=0;
+int P[22];
+bool visit[22]={false};
+void show_q(int index){
+    if(index==N+1){
+        int flag=true;
+        for(int i=1;i<N;i++){
+            for(int j=i+1;j<=N;j++){
+                if(abs(j-i)==abs(P[j]-P[i])) //i P[i] 分别对应一个点的横坐标和纵坐标
+                    flag=false;
             }
         }
-        tp+=((max_amount-m)>k[pos]?k[pos]:max_amount-m)*s[pos];
-        m+=(max_amount-m)>k[pos]?k[pos]:max_amount-m;
-        s[pos]=0;
-
+        if(flag){
+            counter++;
+        }
+        return;
     }
-    printf("%.2f",tp);
-    /*
-3 20
-18 15 10
-75 72 45
-    */
-
-
+    else{
+        for(int i=1;i<=N;i++){
+            if(!visit[i]){
+                P[index]=i;visit[i]=true;
+                show_q(index+1);
+                visit[i]=false;
+            }
+        }
+    }
+}
+int main(){
+    show_q(1);
+    cout<<counter;
 }
