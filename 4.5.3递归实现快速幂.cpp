e173d95349624a01d222aaa2617184e9f656cd98
Higashi89 <zxd1901@126.com>
2019-08-26 12:42:11 +0800

    zxd

--- 4.5.3递归实现快速幂.cpp
+++ 4.5.3递归实现快速幂.cpp
@@ -6,8 +6,7 @@ typedef long long LL;
 //a^b%m
 
 LL km(LL a,LL b,LL m){
-    if(b==0)return 1;
-    //任何数的零次方都是1 
+    if(b==0)return 1;//任何数的零次方都是1
     else if(b%2==1) return a*km(a,b-1,m)%m;
     else if(b%2==0) return km(a,b/2,m)*km(a,b/2,m)%m;
 }
