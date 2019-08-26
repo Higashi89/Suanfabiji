859ea2e3400c23863698407f9338b52b7c3fc9ca
Higashi89 <zxd1901@126.com>
2019-08-26 12:34:27 +0800

    zxd

--- 4.5.2二分法求根号2的逼近值.cpp
+++ 4.5.2二分法求根号2的逼近值.cpp
@@ -6,7 +6,7 @@ double f(double x){
     return x*x; //参数一开始写成了int报错
 }
 int main(){
-    double left=0,right=2,mid; //一开始习惯性声明成了int报错
+    double left=0,right=2,mid; //一开始习惯性声明成了int报错 
     while(right-left>minn){
         mid=(right+left)/2;
         if(f(mid)<2)left=mid;
