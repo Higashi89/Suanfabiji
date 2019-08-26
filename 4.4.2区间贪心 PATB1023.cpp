2bb560e79c4218a38cb9e991a55ceffee897baa8
Higashi89 <zxd1901@126.com>
2019-08-26 10:53:05 +0800

    zxd

--- 4.4.2区间贪心 PATB1023.cpp
+++ 4.4.2区间贪心 PATB1023.cpp
@@ -1,38 +1,28 @@
 #include<cstdio>
-#include<algorithm>
 #include<iostream>
 using namespace std;
-struct node{
-    int left,right;
-}a[100];
-bool cmp(node n1,node n2){
-    if(n1.right!=n2.right)return n1.right<n2.right;
-    else return n1.left>n2.left;
-}
 int main(){
-    int k=0;
-    while(scanf("%d %d",&a[k].left,&a[k].right)!=EOF){//!=EOF必须要加上 最后用ctrl+z结束输入
-        k++;
+    int a[10]={0};
+    int x;
+    for(int i=0;i<10;i++){
+        cin>>x;
+        a[i]=x;
     }
-
-    sort(a,a+k,cmp);
-    int t_left=0,t_right=0,ans=0;
-    for(int i=0;i<k;i++){
-        if(a[i].left<t_right)continue;
-        else{
-            t_left=a[i].left;
-            t_right=a[i].right;
-            ans++;
-            cout<<a[i].left<<" "<<a[i].right<<endl;
+    int first=0;
+    for(int i=1;i<10;i++){
+        if(a[i]!=0){
+            first=i;
+            break;
         }
     }
-    cout<<ans;
-
+    cout<<first;//只用输1位除0以外的最小数就行，剩下的在0之后输出
+    for(int i=0;i<10;i++){
+        if(i!=first) //一开始把这个判断放在for里面了导致到first循环中断出错
+            for(int j=0;j<a[i];j++)
+                cout<<i;
+        else
+            for(int j=0;j<a[first]-1;j++)
+                cout<<first;
+    }
+    //2 2 0 0 0 3 0 0 1 0
 }
-/*
-4
-1 3
-2 4
-3 5
-6 7
-*/
