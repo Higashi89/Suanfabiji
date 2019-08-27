3c702bc26a4ef328252d4d07b7e162cb5497ee60
Higashi89 <zxd1901@126.com>
2019-08-27 14:40:08 +0800

    zxd

---
+++ 4.6.3使用双指针法和 递归实现快速排序 不引入随机 on2复杂度.cpp
@@ -0,0 +1,33 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+int q_change(int a[],int L,int R){
+    int temp=a[L];
+    while(L<R){
+        while(L<R&&a[R]>temp)R--;
+        a[L]=a[R];
+        while(L<R&&a[L]<temp)L++;
+        a[R]=a[L];
+    }
+    a[L]=temp;
+    return L;
+}
+
+void q_sort(int a[],int L,int R){
+    cout<<L<<" "<<R<<endl;
+    if(L<R){
+        int pos=q_change(a,L,R);
+        q_sort(a,L,pos-1);
+        q_sort(a,pos+1,R); // 原本写成 q_sort(a,pos,R);出错
+    }
+}
+
+int main(){
+    int a[11]={35,18,16,72,24,65,12,88,45,28,55};
+    q_sort(a,0,10);
+    for(int i=0;i<11;i++)
+        cout<<a[i]<<" ";
+}
